e2bb7a1d6ff9c218c0fda36197518b13aec0bd2c
shuchao <645904877@qq.com>
2017-01-16 02:04:17 +0800

    add comment

--- main.go
+++ main.go
@@ -8,15 +8,15 @@ import (
 )
 
 func main() {
-    bytes, _ := collector.Start()
-    log.Println(string(bytes))
     Daemon := &daemon.Daemon{
         LogFile: "/var/log/monitord.log",
     }
     Daemon.Start(0, 0)
     for {
+        log.Println("hello monitor")
         time.Sleep(10 * time.Second)
         bytes, _ := collector.Start()
         log.Println(string(bytes))
+        
     }
 }
