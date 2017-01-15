53d64918f21fa33be0f1a467e0e0747b4e97e5be
shuchao <645904877@qq.com>
2017-01-16 01:54:39 +0800

    add comment

--- main.go
+++ main.go
@@ -2,20 +2,19 @@ package main
 
 import (
     "log"
-    //"time"
-    //"monitor/daemon"
+    "time"
+    "monitor/daemon"
     "monitor/collector"
 )
 
 func main() {
-    //Daemon := &daemon.Daemon{
-    //    LogFile: "/var/log/monitord.log",
-    //}
-    //Daemon.Start(0, 0)
-    //for {
-    //    time.Sleep(1 * time.Second)
-    //    log.Println("hello monitor")
-    //}
-    bytes, _ := collector.Start()
-    log.Println(string(bytes))
+    Daemon := &daemon.Daemon{
+        LogFile: "/var/log/monitord.log",
+    }
+    Daemon.Start(0, 0)
+    for {
+        time.Sleep(10 * time.Second)
+        bytes, _ := collector.Start()
+        log.Println(string(bytes))
+    }
 }
