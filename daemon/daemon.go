2ff85c535da92f336b2aa51f70ad2718b4077e15
shuchao <645904877@qq.com>
2017-01-17 21:33:39 +0800

    add comment

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -14,7 +14,6 @@ type Daemon struct {
 }
 
 func (D *Daemon) Daemon() {
-    
     File, err := os.OpenFile(D.LogFile, os.O_RDWR | os.O_CREATE, 0644)
     if err != nil {
         fmt.Printf("create log error: %v\r\n", err)
@@ -38,28 +37,32 @@ func (D *Daemon) Daemon() {
         return
     }
     if _, err = File.WriteString(fmt.Sprint(os.Getpid())); err != nil {
-        log.Printf("fail write pid to %s: %v", D.PidFile, err)
+        log.Printf("fail write pid to %s: %v\r\n", D.PidFile, err)
         return
     }
     Signal := make(chan os.Signal, 1)
-    signal.Notify(Signal, syscall.SIGTERM, os.Interrupt, syscall.SIGUSR2)
+    signal.Notify(Signal, syscall.SIGTERM, syscall.SIGKILL, os.Interrupt)
     for {
-        fmt.Println("+++++++go forever")
         switch <-Signal {
-        case syscall.SIGTERM:
-            fmt.Println("安全退出")
-            Exit(File)
-            os.Exit(1)
-        case syscall.SIGUSR2:
-            fmt.Println("自定义型号.")
-        case os.Interrupt:
-            fmt.Println("安全退出")
-            Exit(File)
+        case syscall.SIGTERM, syscall.SIGKILL, os.Interrupt:
+            if err := D.ClearPidFile(File); err == nil {
+                fmt.Println("bye bye!")
+                os.Exit(1)
+            } else {
+                fmt.Printf("fail to exit proc: %v\r\n", err)
+            }
+        default:
+            fmt.Println("unknow signal, this process will go on...")
         }
     }
 }
 
-func Exit(F *os.File) {
-    F.Close()
-    os.Remove(F.Name())
+func (D *Daemon) ClearPidFile(F *os.File) (error) {
+    if err := F.Close(); err != nil {
+        return err
+    }
+    if err := os.Remove(F.Name()); err != nil {
+        return err
+    }
+    return nil
 }
