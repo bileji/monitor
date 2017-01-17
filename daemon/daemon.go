36ecba396f7e7756fbbc7f43d0be9aeccc652b06
shuchao <645904877@qq.com>
2017-01-17 21:11:12 +0800

    add comment

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -4,76 +4,58 @@ import (
     "os"
     "log"
     "syscall"
-    "errors"
-    "strconv"
     "os/signal"
     "fmt"
 )
 
 type Daemon struct {
-    LogFile    string
-    PidFile    string
-    PidHandler *os.File
+    LogFile string
+    PidFile string
 }
 
-func (d *Daemon) Start(ChDir, Close int) (int, error) {
-    var err error
+func (D *Daemon) Daemon() {
     
-    // 判断是否已有程序启动 pid
-    d.PidHandler, err = os.OpenFile(d.PidFile, os.O_RDWR | os.O_CREATE, 0644)
+    File, err := os.OpenFile(D.LogFile, os.O_RDWR | os.O_CREATE, 0644)
     if err != nil {
-        return -1, err
+        fmt.Printf("create log error: %v\r\n", err)
+        return
     }
-    Info, _ := d.PidHandler.Stat()
-    if Info.Size() != 0 {
-        return -1, errors.New("pid file is exist:" + d.PidFile)
-    }
+    log.SetOutput(File)
     
-    // 已经以daemon启动
+    File, err = os.OpenFile(D.PidFile, os.O_RDWR | os.O_CREATE, 0644)
+    if err != nil {
+        log.Printf("read pid file error: %v\r\n", err)
+        return
+    }
+    Info, _ := File.Stat()
+    if Info.Size() != 0 {
+        log.Printf("pid file is exist: %s\r\n", D.LogFile)
+        return
+    }
     if os.Getppid() != 1 {
         args := append([]string{os.Args[0]}, os.Args[1:]...)
         os.StartProcess(os.Args[0], args, &os.ProcAttr{Files: []*os.File{os.Stdin, os.Stdout, os.Stderr}})
-        return 0, nil
+        return
     }
-    
-    d.PidHandler.WriteString(strconv.Itoa(os.Getpid()))
-    
-    // 处理退出信号
+    File.WriteString(fmt.Sprint(os.Getpid()))
     Signal := make(chan os.Signal, 1)
-    signal.Notify(Signal, os.Interrupt, syscall.SIGUSR2)
+    signal.Notify(Signal, syscall.SIGTERM, os.Interrupt, syscall.SIGUSR2)
     for {
-        C := <-Signal
-        fmt.Println(C)
-        switch C {
-        case os.Interrupt:
-            d.Exit(d.PidHandler)
-            log.Println("exit success")
+        switch <-Signal {
+        case syscall.SIGTERM:
+            fmt.Println("安全退出")
+            Exit(File)
+            os.Exit(1)
         case syscall.SIGUSR2:
-            log.Println("to do for user signal")
+            fmt.Println("自定义型号.")
+        case os.Interrupt:
+            fmt.Println("安全退出")
+            Exit(File)
         }
     }
-    return 0, nil
 }
 
-func (d *Daemon) Exit(F *os.File) {
+func Exit(F *os.File) {
     F.Close()
     os.Remove(F.Name())
 }
-
-//func (d *Daemon) Signal() {
-//    // 处理退出信号
-//    Signal := make(chan os.Signal, 1)
-//    signal.Notify(Signal, os.Interrupt, syscall.SIGUSR2)
-//    for {
-//        C := <-Signal
-//        fmt.Println(C)
-//        log.Println(C)
-//        switch C {
-//        case os.Interrupt:
-//            d.Exit(d.PidHandler)
-//            log.Println("exit success")
-//        case syscall.SIGUSR2:
-//            log.Println("to do for user signal")
-//        }
-//    }
-//}
