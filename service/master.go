b8a06cab4880a47596088f1f475ed3cac8adba1c
shuchao <645904877@qq.com>
2017-01-19 21:11:25 +0800

    add comment

---
+++ service/master.go
@@ -0,0 +1,43 @@
+package service
+
+import (
+    "fmt"
+    "net/http"
+    "encoding/json"
+    "gopkg.in/mgo.v2"
+)
+
+type Answer struct {
+    Code    int32 `json:"code"`
+    Data    map[string]interface{} `json:"data"`
+    Message string `json:"message"`
+}
+
+func (A *Answer)Return(Res http.ResponseWriter) {
+    Res.Header().Set("Content-type", "application/json")
+    Bytes, _ := json.Marshal(A)
+    Res.Write(Bytes)
+}
+
+type Master struct {
+    Addr      string
+    DBHandler *mgo.Database
+}
+
+func Listen(m Master) {
+    http.HandleFunc("/save", m.Save)
+    http.ListenAndServe(m.Addr, nil)
+}
+
+func (m *Master) Save(Res http.ResponseWriter, Req *http.Request) {
+    
+    fmt.Println(Req.Method)
+    fmt.Println("++++++++")
+    fmt.Println(Req.Body)
+    
+    if Req.Method == "PUT" {
+        
+    }
+}
+
+
