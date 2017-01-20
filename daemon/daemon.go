59c295aa3ab1c8d319b0e5c056f86102ee76aaa8
shuchao <645904877@qq.com>
2017-01-21 02:31:03 +0800

    fix bug

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -51,8 +51,7 @@ func (D *Daemon) Daemon(routines... func()) {
         switch <-Signal {
         case syscall.SIGTERM, syscall.SIGKILL, os.Interrupt:
             // todo 疑问为什么关闭pid是阻塞的，而关闭log是非阻塞的
-            LogFile.Close()
-            os.Remove(LogFile.Name())
+            D.ClearFile(LogFile)
             //if err := D.ClearFile(PidFile); err == nil {
             //if err := D.ClearFile(LogFile); err == nil {
             fmt.Println("success to exit proc, bye bye!")
