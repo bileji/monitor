d50b5673a94eaa41cb9ec2bb7bbd1cffe96443dc
shuchao <645904877@qq.com>
2017-02-05 16:26:50 +0800

    ...

---
+++ cmd/monitor.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "os"
+    "fmt"
+    "github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+    Use: "monitord",
+    Short: "Linux server status monitor",
+    Long: "",
+    Run: func(cmd *cobra.Command, args []string) {
+        // TODO ...
+    },
+}
+
+func addCommand(RootCmd *cobra.Command) {
+    //RootCmd.AddCommand(serverCmd)
+}
+
+func Execute() {
+    addCommand(rootCmd)
+    if err := rootCmd.Execute(); err != nil {
+        fmt.Printf("%v", err)
+        os.Exit(1)
+    }
+}
+
+// todo add flags
+func init() {
+    
+}
---
+++ cmd/monitord.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+    "os"
+    "fmt"
+    "net"
+    "runtime"
+    "monitor/daemon"
+    "github.com/spf13/cobra"
+)
+
+type DBAuth struct {
+    Host     string `json:"host"`
+    Port     int `json:"port"`
+    Database string `json:"database"`
+    Username string `json:"username"`
+    Password string `json:"password"`
+}
+
+func main() {
+    runtime.GOMAXPROCS(runtime.NumCPU())
+    
+    RootCmd := &cobra.Command{
+        Use: "monitord",
+        Short: "monitor daemon",
+        // todo ...
+        Long: "to do...",
+        RunE:func(cmd *cobra.Command, args []string) {
+            
+        },
+    }
+    
+    if err := RootCmd.Execute(); err != nil {
+        // todo
+        fmt.Printf("%v", err)
+        os.Exit(-1)
+    }
+    
+    Daemon := &daemon.Daemon{
+        PidFile: "/var/run/monitord.pid",
+        UnixFile: "/var/run/monitord.sock",
+        LogFile: "/var/log/monitord.log",
+    }
+    
+    Daemon.Daemon(func(ch chan []byte, wr *net.UnixListener) {
+        
+    })
+}
