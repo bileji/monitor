edb44abd07a64ca611d1ead77642da69c1eed0f0
shuchao <645904877@qq.com>
2017-02-06 18:27:06 +0800

    fix bug

--- cmd/monitord.go
+++ cmd/monitord.go
@@ -91,8 +91,8 @@ func main() {
     // 配置文件路径
     Flags.StringVarP(&ConfFile, "config", "c", "/etc/monitor.yaml", "monitor config file path")
     
-    Flags.StringVarP(&PidFile, "pid_file", "", "/var/run/monitord.pid", "monitor daemon pid file")
-    Flags.StringVarP(&LogFile, "log_file", "l", "/var/log/monitord.log", "monitor daemon log file")
+    Flags.StringVarP(&PidFile, "pid", "", "/var/run/monitord.pid", "monitor daemon pid file")
+    Flags.StringVarP(&LogFile, "log", "l", "/var/log/monitord.log", "monitor daemon log file")
     // todo socket file and mongodb config
     
     Viper.BindPFlag("server.pid_file", Flags.Lookup("pid_file"))
