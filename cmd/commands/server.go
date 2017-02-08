b634e6148a08be24363ac9b950a2d9cc5d81c127
shuchao <645904877@qq.com>
2017-02-08 16:01:11 +0800

    fix bug

--- cmd/commands/server.go
+++ cmd/commands/server.go
@@ -72,10 +72,10 @@ var ServerCmd = &cobra.Command{
 func init() {
     Flags := initCmd.Flags()
     
-    Flags.StringVarP(&MongoDB.Host, "host", "h", "127.0.0.1", "mongodb host")
-    Flags.Int32VarP(&MongoDB.Port, "port", "p", 27017, "mongodb port")
-    Flags.StringVarP(&MongoDB.AuthDB, "auth", "a", "admin", "auth database")
-    Flags.StringVarP(&MongoDB.Username, "user", "u", "", "username")
+    Flags.StringVarP(&MongoDB.Host, "host", "", "127.0.0.1", "mongodb host")
+    Flags.Int32VarP(&MongoDB.Port, "port", "", 27017, "mongodb port")
+    Flags.StringVarP(&MongoDB.AuthDB, "auth", "", "admin", "auth database")
+    Flags.StringVarP(&MongoDB.Username, "user", "", "", "username")
     Flags.StringVarP(&MongoDB.Password, "pwd", "", "", "password")
     
     Viper.BindPFlag("mongodb.host", Flags.Lookup("host"))
