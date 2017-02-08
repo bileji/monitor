33347107bc1e74eaba675d615d40e8464ca73efb
shuchao <645904877@qq.com>
2017-02-08 16:02:00 +0800

    fix bug

--- cmd/commands/server.go
+++ cmd/commands/server.go
@@ -70,7 +70,7 @@ var ServerCmd = &cobra.Command{
 }
 
 func init() {
-    Flags := initCmd.Flags()
+    Flags := ServerCmd.Flags()
     
     Flags.StringVarP(&MongoDB.Host, "host", "", "127.0.0.1", "mongodb host")
     Flags.Int32VarP(&MongoDB.Port, "port", "", 27017, "mongodb port")
