78e6a31da42e76d0e35eebc1593a097cddcab583
shuchao <645904877@qq.com>
2017-02-13 14:17:42 +0800

    fix bug

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -52,37 +52,3 @@ func (m *Monitor) SInit(Msg []byte) error {
         return nil
     }
 }
-
-//type WebServer struct {
-//    Addr     string                 `json:"addr"`
-//    Database configures.Database    `json:"database"`
-//    Token    string                 `json:"token"`
-//}
-//
-//// 初始化服务
-//func (m *Monitor) ServerInit(WS *WebServer) error {
-//
-//    Session, err := mgo.Dial(WS.Database.Host + ":" + strconv.Itoa(int(WS.Database.Port)))
-//    if err != nil {
-//        return err
-//    }
-//
-//    err = Session.DB(WS.Database.AuthDB).Login(WS.Database.Username, WS.Database.Password)
-//    if err != nil {
-//        return err
-//    }
-//
-//    go (&service.Master{
-//        Addr: WS.Addr,
-//        DBHandler: Session.DB(WS.Database.AuthDB),
-//        Token: WS.Token,
-//    }).Listen()
-//
-//    return nil
-//}
-//
-//// node join
-//func (m *Monitor) Join() error {
-//
-//    return nil
-//}
